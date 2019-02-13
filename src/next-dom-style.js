1e0145c73cb4e6334c8226b6bca2371e38830f7e
afeiship <1290657123@qq.com>
2019-02-13 22:50:21 +0800

    feat: set/sets done

--- src/next-dom-style.js
+++ src/next-dom-style.js
@@ -3,17 +3,36 @@
   var nx = global.nx || require('next-js-core2');
   var nxCamelize = nx.camelize || require('next-camelize');
   var NxCssText = nx.CssText || require('next-css-text');
+  var nxDasherize = nx.dasherize || require('next-dasherize');
 
   var NxDomStyle = nx.declare('nx.DomStyle', {
     statics: {
-      set: function(inElement, inName, inValue) {},
+      set: function(inElement, inName, inValue) {
+        var result = '';
+        if (!value && value !== 0) {
+          inElement.style.removeProperty(nxDasherize(inName));
+        } else {
+          result = nxDasherize(inName) + ':' + inValue;
+        }
+        inElement.style.cssText += ';' + result;
+      },
       get: function(inElement, inName) {
         return (
           inElement.style[nxCamelize(inName)] ||
           global.getComputedStyle(inElement, '').getPropertyValue(inName)
         );
       },
-      sets: function(inElement, inObject) {},
+      sets: function(inElement, inObject) {
+        var result = '';
+        nx.forIn(inObject, function(key, value) {
+          if (!value && value !== 0) {
+            inElement.style.removeProperty(nxDasherize(key));
+          } else {
+            result += nxDasherize(key) + ':' + inValue;
+          }
+        });
+        inElement.style.cssText += ';' + result;
+      },
       gets: function(inElement) {
         var cssText = inElement.style.cssText;
         return NxCssText.unserialize(cssText);
